9b58709a9222783b3bfa0d3ffa69db4d2f4b46ce
DaviOfAstora <116533169+DaviOfAstora@users.noreply.github.com>
2023-01-10 19:13:21 -0300

    Delete FunTelaGameOver.js

--- FunTelaGameOver.js
+++
@@ -1,9 +0,0 @@
-
-function telaGameOver(){
-  background(imggameover)
-  push()
-  opcaoBotaoCircular(CxvoltarMenu, CyvoltarMenu,d2,raio)
-  opcaoBotaoCircular(Cxagain, Cyagain,d3,raio)
-  pop()
-  
-}
