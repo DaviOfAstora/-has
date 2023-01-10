b7a6d98300f80ec56f7046d7af9bb657a614c915
DaviOfAstora <116533169+DaviOfAstora@users.noreply.github.com>
2023-01-10 19:13:38 -0300

    Delete FunTelaVenceu.js

--- FunTelaVenceu.js
+++
@@ -1,9 +0,0 @@
-function telaVenceu(){
-  background(imgvenceu)
-  
-  push()
-  opcaoBotaoCircular(CxvoltarMenu, CyvoltarMenu,d2,raio)
-  opcaoBotaoCircular(Cxagain, Cyagain,d3,raio)
-  pop()
-  
-}
